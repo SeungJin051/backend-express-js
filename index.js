addb9b5364cec99881f827fbb2cb371c26faa632
SeungJin051 <seungjin051@gmail.com>
2024-08-21 23:29:14 +0900

    Express에서 Hello GET 요청

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const port = 4000;
+
+// port에서 서버 실행
+app.listen(port, () => {
+  console.log(`Ex app listening on prot ${port}`);
+});
+
+// 홈페이지에 GET 요청이 오면 "Hello from Node API"로 응답합니다.
+app.get('/', (req, res) => {
+  res.send("Hello from Node API");
+});
